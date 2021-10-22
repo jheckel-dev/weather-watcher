99b62af88a7c143d87796fe5d4f0892fd75a4fa6
jheckel <jalen.heckel@icloud.com>
2021-10-21 17:36:02 -0700

    added api variables for the weather calls

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ var searchButton =$(".searchButton");
 var apiKey= "b3f03bc91b3503ca26b7aeea2999133d"
 
 for (var i = 0; i < localStorage.length; i++) {
-    var city = localStorage.getItem
+    var city = localStorage.getItem(i);
+    var cityName = $(".list-group").addClass("list-group-item");
+
+    cityName.append("<li>" + city + "</li>");
 }
+
+var keyCount = 0;
+
+searchButton.click(function () {
+    var searchInput = $(".searchInput").val();
+
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+
+    var urlSevenDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+})
