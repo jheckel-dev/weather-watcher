55fb8ef6ddafa72612e8ed67ed45b057fb51bda0
jheckel <jalen.heckel@icloud.com>
2021-10-21 18:17:14 -0700

    added the url for the UV index

--- script.js
+++ script.js
@@ -49,6 +49,8 @@ searchButton.click(function () {
             currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
             // shows wind speed
             currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
+            // adds uv index info to our list
+            var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
 
         })
 
